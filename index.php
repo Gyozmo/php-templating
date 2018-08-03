cc293439e5015c24e57336fcd29a09ad7bd9f5e7
Gyozmo <binantguillaume@gmail.com>
2018-08-03 15:28:09 +0200

    Twig n faker ok

--- index.php
+++ index.php
@@ -2,16 +2,19 @@
 require_once './vendor/autoload.php';
 
 $loader = new Twig_Loader_Filesystem('templates');
+$faker = Faker\Factory::create("fr_FR");
 
 $twig = new Twig_Environment($loader, array(
     'cache' => false,
 ));
 
 
-$template = $twig->load("index.html");
-echo $template -> render(array(
-    "the" => "index",
-    "is" => "here"
+// $template = $twig->load("index.html");
+echo $twig -> render("index.html",array(
+    "fakename" => $faker->name,
+    "jobTitle" => $faker->jobTitle,
+    "company" => $faker->company,
+    "catchPhrase" => $faker->catchPhrase,
 ));
 
 ?>
